15bbd668e4b1c8cc1c5027007764c2495befaf95
Ilshat Fatkhullin <ilshat.fatkhullin@gmail.com>
2018-03-29 01:01:55 +0300

    Initial commit.

---
+++ LISY/LISY/DataManagers/DatabaseDataManager.cs
@@ -0,0 +1,17 @@
+﻿using Dapper;
+using LISY.Helpers;
+using System.Data;
+
+namespace LISY.DataManagers
+{
+    public class DatabaseDataManager
+    {
+        public static void ClearAll()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(DatabaseHelper.GetConnectionString()))
+            {
+                connection.Execute("dbo.spLISy_ClearAll");
+            }
+        }
+    }
+}
---
+++ LISY/LISY/Helpers/DatabaseHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LISY.Helpers
+{
+    public static class DatabaseHelper
+    {
+        public static string GetConnectionString()
+        {
+            return "Server=lisy.database.windows.net;Database=LISy;User Id=librarian;Password=nairarbil0#;";
+        }
+    }
+}
